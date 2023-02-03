4735ca4a7206d275b5c09ecbc4688cd7847a90c4
Eduard <goby145csgo@gmail.com>
2023-02-03 22:17:00 +0100

    little improves

--- src/components/UI/Line.jsx
+++ src/components/UI/Line.jsx
@@ -1,5 +1,5 @@
 const Line = () => {
-   return <div className='w-full h-[1px] mt-4 bg-neutral-200'></div>
+   return <div className='w-full h-[1px] my-4 bg-neutral-200'></div>
 }
 
 export default Line
