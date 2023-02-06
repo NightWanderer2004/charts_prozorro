d98ea05b91354b1c6810ad439425291ab02a902b
Eduard <goby145csgo@gmail.com>
2023-02-06 23:47:52 +0100

    fix files routes

--- src/components/UI/DataChart.jsx
+++
@@ -1,9 +0,0 @@
-import { usePlansQuery } from '../../redux/api/apiSlice'
-
-const DataChart = () => {
-   const { data, isLoading, isSuccess, isError, error } = usePlansQuery()
-
-   return <div>{isLoading ? <div className='animate-pulse rounded-md w-full h-[650px] bg-neutral-300'></div> : console.log(data)} </div>
-}
-
-export default DataChart
