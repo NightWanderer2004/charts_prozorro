835ee6ee388945b78e18a430fda6c2bf6b38c5c0
Eduard <goby145csgo@gmail.com>
2023-02-12 22:07:44 +0100

    update links

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,18 +5,18 @@ const Sidebar = () => {
    return (
       <aside className={s.sidebar}>
          <ul className={s.links}>
-            <PageBtn route='/dashboard'>
+            <PageBtn route='/plans'>
                <svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 0 24 24' width='24px'>
                   <path d='M0 0h24v24H0z' fill='none' />
                   <path d='M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z' />
                </svg>
-               Dashboard
+               Плани
             </PageBtn>
-            <PageBtn route='/orders'>
+            <PageBtn route='/tenders'>
                <svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 0 24 24' width='24px'>
                   <path d='M11 21h-1l1-7H7.5c-.58 0-.57-.32-.38-.66.19-.34.05-.08.07-.12C8.48 10.94 10.42 7.54 13 3h1l-1 7h3.5c.49 0 .56.33.47.51l-.07.15C12.96 17.55 11 21 11 21z' />
                </svg>
-               Tenders
+               Тендери
             </PageBtn>
          </ul>
       </aside>
