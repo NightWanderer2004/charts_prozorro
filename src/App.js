eebcd3bd894229c6f03f8d23782c167447941f45
Eduard <goby145csgo@gmail.com>
2023-02-12 22:06:22 +0100

    remove auto url

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import s from './styles/main.module.scss'
 import Navbar from './components/navbar/Navbar'
 import Sidebar from './components/sidebar/Sidebar'
-import { Outlet, useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
 import Content from './components/UI/Content'
 
 function App() {
-   const navigate = useNavigate()
-
-   useEffect(() => {
-      navigate('/dashboard')
-   }, [])
-
    return (
       <div className='min-h-screen overflow-hidden'>
          <Navbar />
